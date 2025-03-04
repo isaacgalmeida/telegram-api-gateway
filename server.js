8eb5dcd6da7e8ce7800f9d43c65b56ef2393f4cd
Isaac Almeida <isaacgalmeida@hotmail.com>
2025-03-04 16:13:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(authenticate);
 const apiId = parseInt(process.env.TELEGRAM_API_ID.trim());
 const apiHash = process.env.TELEGRAM_API_HASH.trim();
 const stringSession = new StringSession(process.env.STRING_SESSION.trim());
+console.log(stringSession)
 const channels = process.env.CHANNELS_SOURCE.split(",");
 const channelTarget = process.env.CHANNEL_TARGET;
 
