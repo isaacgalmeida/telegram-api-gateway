5487fe58c9e59ae8fb15c9367b62b855624a1151
Isaac Almeida <isaacgalmeida@hotmail.com>
2025-03-04 16:41:23 -0300

    Update session.js

--- session.js
+++ session.js
@@ -2,7 +2,7 @@ import { TelegramClient } from "telegram";
 import { StringSession } from 'telegram/sessions/index.js';
 import readline from "readline";
 
-const apiId = parseInt(process.env.TELEGRAM_API_ID.trim());
+const apiId = parseInt(process.env.TELEGRAM_API_ID);
 const apiHash = process.env.TELEGRAM_API_HASH.trim();
 const stringSession = new StringSession(process.env.STRING_SESSION.trim()); // fill this later with the value from session.save()
 
