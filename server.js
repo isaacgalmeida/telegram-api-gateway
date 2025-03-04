9a65d8660bd42bc645ea22c6a2a893e9cc99e8a2
Isaac Almeida <isaacgalmeida@hotmail.com>
2025-03-04 16:20:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ require("dotenv").config();
 // Variáveis do Telegram
 const apiId = parseInt(process.env.TELEGRAM_API_ID.trim());
 const apiHash = process.env.TELEGRAM_API_HASH.trim();
-const stringSession = new StringSession(process.env.STRING_SESSION.trim());
-console.log(stringSession)
+console.log(process.env.STRING_SESSION.trim())
 const channels = process.env.CHANNELS_SOURCE.split(",");
 const channelTarget = process.env.CHANNEL_TARGET;
 
